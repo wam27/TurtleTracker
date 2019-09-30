6284354b2185cb5d71e41743847a7a72e0ff283d
wam27 <wam27@duke.edu>
2019-09-30 12:10:53 -0400

    Add front matter

---
+++ ArgosTracker.py
@@ -0,0 +1,9 @@
+#-------------------------------------------------------------
+# ARGOSTrackingTool.py
+#
+# Description: Parses a line of ARGOS tracking data 
+#
+# Created by: Wilderson Medina (wam27@duke.edu)
+# Created on: Fall 2019
+#--------------------------------------------------------------
+
