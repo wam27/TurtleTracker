b9dc2a9aec48bb5b8ed39d0351b0a29b5a632abb
wam27 <wam27@duke.edu>
2019-10-02 12:13:20 -0400

    Builds dictionaries

--- ArgosTracker.py
+++ ArgosTracker.py
@@ -8,21 +8,28 @@
 #--------------------------------------------------------------
 
 # Create a variable pointing to the file with no header
-fileName = "Data/raw/saraNoHeader.txt"
+fileName = "Data/raw/SaraNoHeader.txt"
 
 # Open the file as a read-only file object
 fileObj = open(fileName, 'r')
 
-#REad Data
+# Read the first line from the open file object
+lineStrings = fileObj.readlines()
+print ("There are {} records in the file".format(len(lineStrings)))
+ 
+# Close the file object
+fileObj.close()
 
-lineString =fileObj.readline()
+# Create empty dictionaries
+dateDict= {}
+locationDict= {}
 
-#While loop
-while lineString:
+# Use a for loop to read each line, one at a time, until the list is exhausted
+for lineString in lineStrings:
 
     # Use the split command to parse the items in lineString into a list object
     lineData = lineString.split("\t")
-    
+
     # Assign variables to specfic items in the list
     recordID = lineData[0]              # ARGOS tracking record ID
     obsDateTime = lineData[2]           # Observation date and time (combined)
@@ -31,9 +38,9 @@ while lineString:
     obsLC = lineData[3]                 # Observation Location Class
     obsLat = lineData[5]                # Observation Latitude
     obsLon = lineData[6]                # Observation Longitude
-    
-    # Print information to the user
-    print ("Record {0} indicates Sara was seen at {1}N and {2}W on {3}".
-           format(recordID,obsLat,obsLat,obsDate))
-    #Go to next line
-    lineString = fileObj.readline()
+
+    # Add values to dictionary 
+    dateDict[recordID]=obsDate
+    locationDict[recordID]=(obsLat,obsLon) 
+# Indicate script is complete
+print ("Finished")
